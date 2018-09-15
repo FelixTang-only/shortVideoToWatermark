37496a398f54f92d5369bfe98a2cda3fb753ab5d
hzhuangwei <hzhuangwei@corp.netease.com>
2018-09-15 20:45:07 +0800

    misc

--- config/install.php
+++ config/install.php
@@ -135,7 +135,33 @@ if (isset($_POST['install'])) { //安装
 <head>
     <meta charset="utf-8">
     <title>后台管理</title>
-    <?php include_once "../meta.php"; ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../static/css/site.css?v32" rel="stylesheet">
+    <link rel="shortcut icon" href="../static/img/favicon.ico">
+    <style type="text/css">
+        .container .navbar-header .logo-img {
+            width: 52px;
+            height: 52px;
+            margin-left: 15px;
+            float: left;
+            display: inline-block;
+            background: url(../static/img/logo.png) no-repeat;
+            background-size: contain;
+            background: 0 0\9;
+            filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='../static/img/logo.png', sizingMethod='scale');
+        }
+        @media (min-width: 768px) {
+            .container .navbar-header .logo-img {margin-left: 0;}
+        }
+    </style>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://cdn.staticfile.org/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdn.staticfile.org/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 
@@ -201,7 +227,7 @@ if (isset($_POST['install'])) { //安装
 
 <script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
 <script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<script src="static/js/vue.min.js"></script>
+<script src="../static/js/vue.min.js"></script>
 <script>
     var app = new Vue({
         el: '#app',
