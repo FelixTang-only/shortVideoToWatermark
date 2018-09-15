29761fb630084d52550cb2bfaac43ff7e227cc45
hzhuangwei <hzhuangwei@corp.netease.com>
2018-09-15 22:30:58 +0800

    misc

--- class/DbConfig.php
+++ class/DbConfig.php
@@ -1,9 +1,12 @@
 <?php
 require 'Db.php';
+require '../config/dbconfig.php';
 Db::config( 'driver',   'mysql' );
-Db::config( 'host',     '172.17.27.76'  );  //数据库host
-Db::config( 'database', 'iiilab' );   //数据库名称
-Db::config( 'user',     'iiilabremote');  //数据库用户
-Db::config( 'password', 'sqlmylabiii'  );  //数据库密码
+Db::config( 'host',     DB_HOST);  //数据库host
+Db::config( 'database', DB_NAME);   //数据库名称
+Db::config( 'user',     DB_USER);  //数据库用户
+Db::config( 'password', DB_PWD);  //数据库密码
+Db::config( 'port',     DB_PORT);  //数据库端口
+
 
 //https://github.com/adriengibrat/Simple-Database-PHP-Class
